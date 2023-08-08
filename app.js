711c9055a63ac25fdc4d859d89688082742ca06e
GIMENAGASPAR <gimenagaspar7@gmail.com>
2023-08-08 20:05:30 -0300

    EJERCITACION CLASE 23

--- app.js
+++ app.js
@@ -4,12 +4,16 @@ const fs=require("fs");
 
 const app=express();
 const publicPath=path.resolve(__dirname, "./public");
+const port=process.env.PORT || 3001;
 
 app.use(express.static(publicPath));
+// app.set("puerto", process.env.PORT || 3001)
 
-app.listen(3000, ()=>{
+/* app.listen(3000, ()=>{
     console.log("servidor corriendo en el puerto 3000");
-});
+}); */
 app.get("/", (req, res)=>{
     res.sendFile(path.resolve(__dirname, "./views/home.html"))
 });
+
+app.listen(port, ()=> console.log('servidor corriendo en el puerto ${port}'));
