2ceca7a149cb9afca39850e4c45309c66f7dc6be
GIMENAGASPAR <gimenagaspar7@gmail.com>
2023-08-10 13:26:08 -0300

    implementacion de redireccion de paginas login y register

--- app.js
+++ app.js
@@ -15,5 +15,11 @@ app.use(express.static(publicPath));
 app.get("/", (req, res)=>{
     res.sendFile(path.resolve(__dirname, "./views/home.html"))
 });
+app.get("/register", (req, res)=>{
+    res.sendFile(path.resolve(__dirname, "./views/register.html"))
+});
+app.get("/login", (req, res)=>{
+    res.sendFile(path.resolve(__dirname, "./views/login.html"))
+});
 
 app.listen(port, ()=> console.log('servidor corriendo en el puerto ${port}'));
